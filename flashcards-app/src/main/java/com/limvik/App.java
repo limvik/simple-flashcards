e1f25dcede4143c24dc79afec394e6a364b1f997
limvik <seongguk.dev@gmail.com>
2023-04-01 14:44:57 +0900

    Create maven project

---
+++ flashcards-app/src/main/java/com/limvik/App.java
@@ -0,0 +1,13 @@
+package com.limvik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
