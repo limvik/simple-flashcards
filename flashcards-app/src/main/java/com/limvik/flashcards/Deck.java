d71caf3feb0148aac5e93d5fc1407bddd8afb5bf
limvik <seongguk.dev@gmail.com>
2023-04-09 19:10:51 +0900

    Modify Deck, Planner object for Board object

--- flashcards-app/src/main/java/com/limvik/flashcards/Deck.java
+++ flashcards-app/src/main/java/com/limvik/flashcards/Deck.java
@@ -5,22 +5,22 @@ import java.util.List;
 public class Deck {
 
     private int id; // 보관함 식별자
-    private String name;
+    private String name; // 보관함 이름
     private int parentId; // 부모 보관함 식별자
     private List<Integer> childIds; // 자식 보관함 식별자 목록
     private List<Integer> cardIds; // 포함한 카드 식별자 목록
 
     // 보관함을 관리하라(생성)
-    public Deck(String name) { }
+    public Deck(String name, int parentId) { }
     
-    // 보관함을 관리하라(수정)
-    public boolean changeParentDeck() { }
+    // 보관함을 관리하라(이동)
+    public boolean changeParentDeck(int parentId) { }
     
     // 보관함을 관리하라(삭제)
-    public boolean removeDeck(int deckId) { }
+    public boolean removeDeck() { }
     
     // 카드를 분류하라
-    public boolean classifyCards(int deckId, int cardId) { }
+    public boolean classifyCards(int cardId, int to) { }
     
     // 카드를 찾아라(키워드 검색)
     public List<Card> findCards(String keywords) { }
@@ -31,8 +31,5 @@ public class Deck {
     public boolean modifyDeckName(int deckId, String name) {
         // 데이터베이스 수정
     }
-
-    // 보관함 목록을 찾아라
-    public List<Deck> getDeckList() { }
 
 }
--- flashcards-app/src/main/java/com/limvik/flashcards/Planner.java
+++ flashcards-app/src/main/java/com/limvik/flashcards/Planner.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Planner {
 
     // 학습일정을 찾아라
-    public List<Plan> searchStudyPlan(LocalDate startDate, LocalDate endDate) {
+    public List<Plan> searchStudyPlan(int userId, LocalDate startDate, LocalDate endDate) {
         // 데이터베이스에서 불러오기
     }
 
     // 학습일정을 기록하라
-    public boolean writePlan(Plan plan) {
+    public boolean writePlan(int userId, Plan plan) {
         // 데이터베이스에 저장
     }
 
