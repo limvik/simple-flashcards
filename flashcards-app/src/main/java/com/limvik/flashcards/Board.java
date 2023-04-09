55b8f1e635dd758d4842a3f717ece1eef849cb99
limvik <seongguk.dev@gmail.com>
2023-04-09 16:35:18 +0900

    Update Board object due to design changes

--- flashcards-app/src/main/java/com/limvik/flashcards/Board.java
+++ flashcards-app/src/main/java/com/limvik/flashcards/Board.java
@@ -7,17 +7,45 @@ public class Board {
 
     private List<Deck> decks; // 보관함 목록
     private List<Plan> plans; // 학습 일정 목록
+    private List<User> users; // 사용자 목록
 
-    // 보관함 목록 및 보관함 별 학습 대상 카드의 개수를 보여라
-    public void showBoard(Deck deck, Planner planner) {
+    // 사용자 목록을 보여라
+    public void showUserList() {
 
-        // 보관함 목록 불러오기
-        decks = deck.getDeckList();
+        // 사용자 목록 불러오기
+        users = /* 데이터베이스 호출 */;
+
+        // 사용자 목록 시현하기
+
+    }
+
+    // 보관함 목록 및 보관함 별 학습 대상 카드 갯수를 보여라
+    public void showBoard(int userSequenceNumber, Planner planner) {
+
+        // 선택된 사용자 정보 식별
+        User user = users.get(userSequenceNumber);
+
+        // 사용자의 보관함 전체 목록 불러오기
+        decks = /* 데이터베이스 호출 */;
 
         // 오늘 학습 일정 불러오기
         LocalDate today = LocalDate.now();
-        plans = planner.searchStudyPlan(today, today);
+        plans = planner.searchStudyPlan(user.getId(), today, today);
 
+        // 보관함 목록 및 보관함 별 학습 대상 카드 갯수 시현하기
+
+    }
+
+    public void setDecks(List<Deck> decks) {
+        this.decks = decks;
+    }
+
+    public void setPlans(List<Plan> plans) {
+        this.plans = plans;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
 }
