6eeb4c226f811ad4748d8463fa00e1a1764dc214
limvik <seongguk.dev@gmail.com>
2023-04-11 22:37:12 +0900

    Refactor Card Class: constructor and methods

--- flashcards-app/src/main/java/com/limvik/flashcards/Card.java
+++ flashcards-app/src/main/java/com/limvik/flashcards/Card.java
@@ -5,17 +5,30 @@ public class Card {
     private int id; // 카드 식별자
     private String front; // 카드 앞면 내용
     private String back; // 카드 뒷면 내용
-    
-    // 카드를 생성하라
-    public Card() { }
+
+    // 새로운 카드를 생성하라
+    public Card(String front, String back) {
+        this.front = front;
+        this.back = back;
+     }
     
     // 학습내용을 기록하라
-    public boolean writeContents(Card card) {
+    public boolean writeContents() {
         // 데이터베이스에 기록
     }
     
     // 카드 정보를 찾아라
-    public Card findCardContents(int cardId) { }
+    public int getId() {
+        return id;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public String getBack() {
+        return back;
+    }
     
     // 카드 앞면 내용 설정
     public void setFront(String front) {
