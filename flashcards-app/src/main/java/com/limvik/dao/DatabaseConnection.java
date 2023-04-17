d8331045722e23b5e3219e58feca8009a9194526
limvik <seongguk.dev@gmail.com>
2023-04-17 22:19:03 +0900

    Add database connection class and test

---
+++ flashcards-app/src/main/java/com/limvik/dao/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package com.limvik.dao;
+
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() throws SQLException {
+        String dbPath = Paths.get("src", "main", "resources", "db", "flashcards.db").toString();
+        this.connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null || instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        } 
+
+        return instance;
+    
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }    
+    }
+}
