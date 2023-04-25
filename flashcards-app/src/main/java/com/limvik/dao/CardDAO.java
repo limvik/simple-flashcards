e3e6a8dc4fdf53d3b64c6e9b65af47031c50933d
limvik <seongguk.dev@gmail.com>
2023-04-25 23:28:56 +0900

    Add get cards for start study session

--- flashcards-app/src/main/java/com/limvik/dao/CardDAO.java
+++ flashcards-app/src/main/java/com/limvik/dao/CardDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.limvik.flashcards.Card;
+import com.limvik.flashcards.Deck;
 
 public class CardDAO {
     private Connection connection;
@@ -32,7 +35,7 @@ public class CardDAO {
     public int insertCard(int userId, int deckId, Card card) {
 
         // 신규 사용자 추가를 위한 SQL
-        String sql = "INSERT INTO Cards(id, user_id, deck_id, front, back, card_type)" +
+        String sql = "INSERT INTO Cards(id, user_id, deck_id, front, back, card_type) " +
                      "VALUES (?, ?, ?, ?, ?, ?)";
         int rowCount = 0;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -67,5 +70,34 @@ public class CardDAO {
         }
         return count;
 
+    }
+
+    public List<Card> getCards(List<Deck> decks) {
+        List<Card> cards = new ArrayList<>();
+
+        // 보관함 내의 카드를 얻기 위한 sql
+        String sql = "SELECT id, user_id, deck_id, front, back, memo, card_type FROM Cards " +
+                     "WHERE deck_id IN (?";
+        if (decks.size() > 1) sql += ", ?".repeat(decks.size() - 1);
+        sql += ")";
+        // SQL 실행 및 데이터 저장
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            for (int i = 1; i <= decks.size(); i++) {
+                statement.setInt(i, decks.get(i-1).getId());
+            }
+            ResultSet resultSet = statement.executeQuery();
+            cards.add(new Card(resultSet.getInt("id"),
+                               resultSet.getInt("user_id"),
+                               resultSet.getInt("deck_id"),
+                               resultSet.getString("front"),
+                               resultSet.getString("back"),
+                               resultSet.getString("memo"),
+                               resultSet.getInt("card_type")));
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return cards;
     }
 }
--- flashcards-app/src/main/java/com/limvik/flashcards/Card.java
+++ flashcards-app/src/main/java/com/limvik/flashcards/Card.java
@@ -8,8 +8,11 @@ import com.limvik.dao.DatabaseConnection;
 public class Card {
 
     private int id; // 카드 식별자
+    private int usreId; // 사용자 식별자
+    private int deckId; // 보관함 식별자
     private String front; // 카드 앞면 내용
     private String back; // 카드 뒷면 내용
+    private String memo; // 카드 추가 메모
     private int cardType; // 카드 종류
 
     // 새로운 카드를 생성하라
@@ -18,10 +21,13 @@ public class Card {
         this.back = back;
     }
 
-    public Card(int id, String front, String back, int cardType) {
+    public Card(int id, int userId, int deckId, String front, String back, String memo, int cardType) {
         this.id = id;
+        this.usreId = userId;
+        this.deckId = deckId;
         this.front = front;
         this.back = back;
+        this.memo = memo;
         this.cardType = cardType;
     }
     
